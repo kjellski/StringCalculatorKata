5bd368faab5d9a3bd687b77d2d96d6587e257b08
@kjellski <otto.kjell@gmail.com>
2015-04-13 23:38:02 +0200

    added paket added xunit + runner added Testclass and Class to test ;D

---
+++ StringCalculatorKata/StringCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Add_Adds_Given_Numbers()
+        {
+            var stringCalculator = new StringCalculator();
+            Assert.Equal(1, stringCalculator.Add("1"));
+        }
+    }
+}
